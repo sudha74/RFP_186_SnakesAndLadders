bec71d7c56674e3816fe3552009a25d0f32d515d
sudha123 <sudhanaukala1997@gmail.com>
2022-08-22 08:35:35 +0530

    initialised_position

---
+++ src/com/bridgelabz/SnakeAndLadders/SnakesAndLadders.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.SnakeAndLadders;
+
+public class SnakesAndLadders {
+    static final int START_POSITION = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snakes And Ladders");
+    }
+}
