a91c29d0d299e605c7c6a961e697c27727e5a729
sudha123 <sudhanaukala1997@gmail.com>
2022-08-22 14:50:49 +0530

    Exact100

--- src/com/bridgelabz/SnakeAndLadders/SnakesAndLadders.java
+++ src/com/bridgelabz/SnakeAndLadders/SnakesAndLadders.java
@@ -9,23 +9,25 @@ public class SnakesAndLadders {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snakes And Ladders");
-        int dienum = (int) (Math.random() * 10) % 6 + 1;
-        int option = (int) (Math.random() * 10) % 3;
-        if (option == IF_LADDER) {
-            currentPosition += dienum;
-        } else if (option == IF_SNAKE) {
-            currentPosition -= dienum;
-        } else if (option == NO_PLAY) {
-            System.out.println("No Play");
+        while (currentPosition < 100) {
+            int dienum = (int) (Math.random() * 10) % 6 + 1;
+            int option = (int) (Math.random() * 10) % 3;
+            if (option == IF_LADDER) {
+                currentPosition += dienum;
+            } else if (option == IF_SNAKE) {
+                currentPosition -= dienum;
+            } else if (option == NO_PLAY) {
+                System.out.println("No Play");
+            }
+            if (currentPosition < START_POSITION) {
+                currentPosition = 0;
+            }
+            if (currentPosition > 100) {
+                currentPosition -= dienum;
+            }
+            System.out.println(currentPosition);
         }
-        if (currentPosition < START_POSITION) {
-            currentPosition = 0;
-        }
-        if (currentPosition > 100) {
-            currentPosition -= dienum;
-        }
-        System.out.println(currentPosition);
-    }
 
+    }
 }
 
