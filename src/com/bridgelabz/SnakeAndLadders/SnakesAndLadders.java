81eb2813f4f837c2095e01d3ad549f5c9c07d536
sudha123 <sudhanaukala1997@gmail.com>
2022-08-22 14:02:31 +0530

    CheckForOption

--- src/com/bridgelabz/SnakeAndLadders/SnakesAndLadders.java
+++ src/com/bridgelabz/SnakeAndLadders/SnakesAndLadders.java
@@ -2,9 +2,22 @@ package com.bridgelabz.SnakeAndLadders;
 
 public class SnakesAndLadders {
     static final int START_POSITION = 0;
+    static final int NO_PLAY = 0;
+    static final int IF_LADDER = 1;
+    static final int IF_SNAKE = 2;
+    static int currentPosition = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snakes And Ladders");
-        int num = (int) (Math.random() * 10) % 6 + 1;
+        int dienum = (int) (Math.random() * 10) % 6 + 1;
+        int option = (int) (Math.random() * 10) % 3;
+        if (option == IF_LADDER) {
+            currentPosition += dienum;
+        } else if (option == IF_SNAKE) {
+            currentPosition -= dienum;
+        } else if (option == NO_PLAY) {
+            System.out.println("No Play");
+        }
     }
 }
+
