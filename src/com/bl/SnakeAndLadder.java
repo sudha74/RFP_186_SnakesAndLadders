1fabfea9200f7b3d0a7ebac3ad3f6a139f8837c9
sudha123 <sudhanaukala1997@gmail.com>
2022-11-09 13:22:20 +0530

    Addedd:start position

---
+++ src/com/bl/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bl;
+
+public class SnakeAndLadder {
+    static final int START_POSITION = 0;
+
+    public static void main(String[] args) {
+        int position = START_POSITION;
+        System.out.println("current position : " + position);
+    }
+}
